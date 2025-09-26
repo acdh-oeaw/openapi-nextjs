5cd06b31370cf64b68099a440dae84bd5716371f
Stefan Probst <stefanprobst@posteo.de>
2025-09-26 11:41:29 +0200

    fix: don't write output file to disk in generate

--- apps/basic/scripts/generate-api-docs.ts
+++ apps/basic/scripts/generate-api-docs.ts
@@ -1,10 +1,11 @@
+import * as fs from "node:fs/promises";
 import * as path from "node:path";
 
 import { log } from "@acdh-oeaw/lib";
 import { generate as generateApiDocs } from "@acdh-oeaw/openapi-nextjs";
 
 async function generate() {
-	await generateApiDocs({
+	const openapiDoc = await generateApiDocs({
 		directory: path.resolve("app"),
 		info: {
 			title: "Example API",
@@ -12,6 +13,13 @@ async function generate() {
 			description: "Generated OpenAPI spec from Next.js app router endpoints.",
 		},
 		servers: [{ url: "http://localhost:3000", description: "Local development server" }],
+	});
+
+	const outputFolder = path.join(process.cwd(), "public");
+	await fs.mkdir(outputFolder, { recursive: true });
+
+	await fs.writeFile(path.join(outputFolder, "openapi.json"), JSON.stringify(openapiDoc, null, 2), {
+		encoding: "utf-8",
 	});
 }
 
