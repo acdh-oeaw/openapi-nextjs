703ebb46f106a0eb0593560ebcf2bd74c602c253
Stefan Probst <stefanprobst@posteo.de>
2025-10-26 20:51:22 +0100

    chore: allow next.js v16 peer dependency, update deps

---
+++ .changeset/vast-cities-bake.md
@@ -0,0 +1,5 @@
+---
+"@acdh-oeaw/openapi-nextjs": patch
+---
+
+allow next.js v16 peer dependency, update deps
--- apps/basic/scripts/generate-api-docs.ts
+++ apps/basic/scripts/generate-api-docs.ts
@@ -4,7 +4,13 @@ import * as path from "node:path";
 import { log } from "@acdh-oeaw/lib";
 import { generate as generateApiDocs } from "@acdh-oeaw/openapi-nextjs";
 
+const formatters = {
+	duration: new Intl.NumberFormat("en-GB", { minimumFractionDigits: 2, maximumFractionDigits: 2 }),
+};
+
 async function generate() {
+	const start = performance.now();
+
 	const openapiDoc = await generateApiDocs({
 		directory: path.resolve("app"),
 		info: {
@@ -21,6 +27,12 @@ async function generate() {
 	await fs.writeFile(path.join(outputFolder, "openapi.json"), JSON.stringify(openapiDoc, null, 2), {
 		encoding: "utf-8",
 	});
+
+	const end = performance.now();
+	const duration = formatters.duration.format(end - start);
+	const stats = { duration };
+
+	log.success(`Successfully generated api docs in ${stats.duration} ms.`);
 }
 
 generate().catch((error: unknown) => {
