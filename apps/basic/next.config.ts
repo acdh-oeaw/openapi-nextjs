85a19d98f9f6966418343b07b251eb6b137006c6
stefanprobst <stefanprobst@posteo.de>
2025-10-26 20:57:50 +0100

    chore: allow next.js v16 peer dependency, update deps (#3)

--- apps/basic/next.config.ts
+++ apps/basic/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig as Config } from "next";
 
 const config: Config = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
 	typedRoutes: true,
 	typescript: {
 		ignoreBuildErrors: true,
